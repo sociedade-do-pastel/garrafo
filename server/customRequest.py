e3b9798ce8cfce5476b06c460f791298549ec1c9
Guior <guilhermeormond@outlook.com>
2020-10-03 19:17:32 -0300

    Adicionado factory de respostas.

--- server/customRequest.py
+++ server/customRequest.py
@@ -1,4 +1,28 @@
-class Request():
-    def __init__(self):
-        scheme = "{}"
-        
+from abc import ABC, abstractmethod
+
+class RequestFactory:
+    """Design Pattern Factory para devolver custom requests"""
+    @staticmethod
+    def makeRequest(message):
+        if message.method == "GET":
+            return ResponseGet(message)
+
+class Response(ABC):
+    def __init__(self, message):
+        self.protocol = message.protocol
+
+    @abstractmethod
+    def makeResponse(self):
+        pass
+
+class ResponseGet(Response):
+    def __init__(self, message):
+        pass
+
+class ReponsePost(Response):
+    def __init__(self, message):
+        pass
+
+class ResponsePut(Response):
+    def __init__(self, message):
+        pass
