6be438c8927d47cf4a63fad63ad3f736a2ef8d61
Guior <guilhermeormond@outlook.com>
2020-10-08 23:19:20 -0300

    Implementado método GET, corrigido parsing de Message.

--- server/customRequest.py
+++ server/customRequest.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-from errorHandler import *
 from datetime import date, time
 import os
+from errorHandler import *
+
 
 # if there isn`t a root directory, create one
 # raise an exception if that's impossible to do
@@ -9,9 +10,10 @@ def checkRoot(root):
     if not os.path.isdir(root):
         os.mkdir(root)
 
+
 def checkProtocol(protocol_string):
     if protocol_string != "HTTP/1.1":
-        raise HTTPnotSupported 
+        raise HTTPnotSupported
 
 
 class RequestFactory:
@@ -24,24 +26,22 @@ class RequestFactory:
             checkProtocol(message.protocol)
             if message.method == "GET":
                 return ResponseGet(message, server_info)
-            elif message.method == "POST":
-                return ResponsePost(messag, server_info)
-            elif message.method == "PUT":
+            if message.method == "POST":
+                return ResponsePost(message, server_info)
+            if message.method == "PUT":
                 return ResponsePut(message, server_info)
-            else:
-                raise BadRequest
+            raise BadRequest
         except GeneralError as err:
             return ResponseError(message, err, server_info)
 
 class Response(ABC):
     def __init__(self, message, server_info):
-        self.hostname  = server_info[1]
-        self.system    = server_info[0]
+        self.hostname = server_info[1]
+        self.system = server_info[0]
         # Date  = "Date" ":" HTTP-date
         self.date = date.today().strftime("%a, %d %b %Y %H:%M:%S %Z")
         self.response = "HTTP/1.1 {}\n\rServer: {}\n\rContent-Type: {}; charset=utf-8\n\rDate: {}\n\r\n\r{}"
-        
-    
+
     @abstractmethod
     def makeResponse(self):
         pass
@@ -49,9 +49,15 @@ class Response(ABC):
 class ResponseGet(Response):
     def __init__(self, message, server_info):
         super().__init__(message, server_info)
-    
+        with open("./{}".format(message.request), "r") as f:
+            self.archive = f.read()
+        self.makeResponse()
+
     def makeResponse(self):
-        pass
+        self.response = self.response.format("200 OK", self.hostname,
+                                             "text/html", self.date,
+                                             self.archive)
+
 
 class ResponsePost(Response):
     def __init__(self, message, server_info):
@@ -69,8 +75,10 @@ class ResponsePut(Response):
 
 class ResponseError(Response):
     def __init__(self, message, error, server_info):
-       super().__init__(message, server_info)
-       self.makeResponse(error)
+        super().__init__(message, server_info)
+        self.makeResponse(error)
 
     def makeResponse(self, err):
-        self.response = self.response.format(err.error_message, self.hostname, "text/html", self.date, err.error_response)
+        self.response = self.response.format(err.error_message, self.hostname,
+                                             "text/html", self.date,
+                                             err.error_response)
--- server/message.py
+++ server/message.py
@@ -15,12 +15,13 @@ class Message:
             y = x.split(" ")
             if y[0] in ["GET", "POST", "PUT"]:
                 self.method, self.request, self.protocol = y
+                self.protocol = self.protocol[:-1]
             elif y[0] == "Host:":
-                self.host = y[1]
+                self.host = y[1][:-1]
             elif y[0] == "User-Agent:":
                 self.user_agent = y[1:len(y)]
             elif y[0] == "Accept:":
-                self.accept = y[1]
+                self.accept = y[1][:-1]
 
     def __str__(self):
         return f"""Method: {self.method}
